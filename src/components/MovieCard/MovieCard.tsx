438e5db8a1291fd40c4d717e97c842c90a20ccbc
abibflores <gon_abib@hotmail.com>
2025-02-06 21:46:41 -0600

    feature: enhance MovieCard component with image display and episode titles

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,13 +1,20 @@
 import { Movie } from "@/types/movies";
+import { buildPathImageUrl, getEpisodeNumber } from "@/utils/movies";
+import Image from "next/image";
 import Link from "next/link";
 
-export const MovieCard = ({ movie }: { movie: Movie}) => {
-    return (
-        <Link href={`/movie/${movie.episode_id}`} key={movie.episode_id}>
-            <div className="bg-gray-800 p-4 rounded-lg shadow-lg hover:shadow-xl transition duration-300">
-            <h2 className="text-2xl font-semibold">{movie.title}</h2>
-            <p className="text-gray-400">{movie.release_date}</p>
-            </div>
-      </Link>
-    );
-}
+export const MovieCard = ({ movie }: { movie: Movie }) => {
+  const imageUrl = buildPathImageUrl(movie.episode_id);
+  const title = getEpisodeNumber(movie.episode_id);
+
+  return (
+    <Link href={`/movie/${movie.episode_id}`} key={movie.episode_id}>
+      <div className="bg-gray-800 p-4 rounded-lg shadow-lg hover:shadow-xl transition duration-300">
+        <Image src={imageUrl} alt={movie.title} width={400} height={550} />
+        <h2 className="text-2xl font-semibold h-20 m-4">
+          {title}:{movie.title}
+        </h2>
+      </div>
+    </Link>
+  );
+};
---
+++ src/utils/movies.ts
@@ -0,0 +1,17 @@
+const titles: { [key: number]: string } = {
+  1: "Episode I",
+  2: "Episode II",
+  3: "Episode III",
+  4: "Episode IV",
+  5: "Episode V",
+  6: "Episode VI",
+};
+export const buildPathImageUrl = (episodeId: number) => {
+  const url = `/images/${episodeId}.webp`;
+
+  return url;
+};
+
+export const getEpisodeNumber = (episodeId: number): string => {
+  return titles[episodeId];
+};
